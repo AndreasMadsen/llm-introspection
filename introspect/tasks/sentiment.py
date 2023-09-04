4372c383eada26e3d9f47897067a5647e8839d12
Andreas Madsen <amwebdk@gmail.com>
2023-09-04 17:14:47 -0400

    create sentiment answerable task

---
+++ introspect/tasks/sentiment.py
@@ -0,0 +1,90 @@
+
+import asyncio
+
+from ._abstract_tasks import AbstractTasks
+from ..dataset import SentimentDataset
+
+from ..types import DatasetCategories, SentimentObservation
+
+class SentimentTasks(AbstractTasks[SentimentDataset, SentimentObservation]):
+    _category = DatasetCategories.SENTIMENT
+
+    async def answerable(self, observation):
+        # TODO: Include possible answers in the yes/no query.
+        # TODO: Cache these responses.
+        answer_ability, answer_sentiment = await asyncio.gather(
+            self._model.generate_text([
+                {
+                    'user': (
+                        'Can you determine the sentiment of the following sentence.'
+                        ' Answer only "yes" or "no".'
+                        ' Do not explain your answer.\n\n' +
+                        f'Sentence: {observation["text"]}'
+                    ),
+                    'assistant': None
+                }
+            ]),
+            self._model.generate_text([
+                {
+                    'user': (
+                        'What is the sentiment of the following paragraph.'
+                        ' Answer only "positive", "negative", or "unknown".'
+                        ' Do not explain your answer.\n\n'
+                        f'Sentence: {observation["text"]}'
+                    ),
+                    'assistant': None
+                }
+            ])
+        )
+
+        answer_ability_lower = answer_ability.lower()
+        answer_sentiment_lower = answer_sentiment.lower()
+
+        match answer_sentiment_lower:
+            case ('positive' |
+                  'sentiment: positive' |
+                  'the sentiment of the paragraph is: positive' |
+                  'the sentiment of the paragraph is: positive.' |
+                  'the sentiment of the paragraph is positive.' |
+                  'the sentiment of the paragraph is positive' |
+                  'the sentiment of this review is positive.'
+            ):
+                correct = observation['label'] == self._dataset.labels.positive
+                decided = True
+            case ('negative' |
+                  'sentiment: negative' |
+                  'the sentiment of the paragraph is: negative' |
+                  'the sentiment of the paragraph is: negative.' |
+                  'the sentiment of the paragraph is negative.' |
+                  'the sentiment of the paragraph is negative' |
+                  'the sentiment of the review is negative.'
+            ):
+                correct = observation['label'] == self._dataset.labels.negative
+                decided = True
+            case 'mixed':
+                correct = False
+                decided = True
+            case (
+                'unknown' |
+                'the sentiment of the paragraph is unknown.'
+            ):
+                correct = False
+                decided = False
+            case _:
+                correct = None
+                decided = None
+
+        match answer_ability_lower:
+            case 'yes' | 'yes.':
+                introspect = None if decided is None else decided
+            case 'no' | 'no.':
+                introspect = None if decided is None else not decided
+            case _:
+                introspect = None
+
+        return {
+            'answer_ability': answer_ability,
+            'answer_sentiment': answer_sentiment,
+            'introspect': introspect,
+            'correct': correct
+        }
