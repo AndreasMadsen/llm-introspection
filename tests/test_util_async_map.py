5acd31d125fb1a6e3a356788e5e1a01c5ec7cbb2
Andreas Madsen <amwebdk@gmail.com>
2023-09-18 15:39:29 -0400

    minor fixes after review

--- tests/test_util_async_map.py
+++ tests/test_util_async_map.py
@@ -17,12 +17,12 @@ async def test_async_map_jobs_run_in_parallel():
         return job_id
 
     iterable = AsyncMap(worker, [1, 2, 3], max_tasks=3)
-    await asyncio.sleep(0)  # allow time for tasks to start the coro
+    await asyncio.sleep(0.01)  # allow time for tasks to start the coro
     assert started == set([])
     assert finished == set([])
 
     iterator = aiter(iterable)
-    await asyncio.sleep(0)  # allow time for tasks to start the coro
+    await asyncio.sleep(0.01)  # allow time for tasks to start the coro
     assert started == {1, 2, 3}
     assert finished == set([])
 
@@ -44,12 +44,12 @@ async def test_async_map_jobs_throttle():
         return job_id
 
     iterable = AsyncMap(worker, [1, 2, 3], max_tasks=1)
-    await asyncio.sleep(0)  # allow time for tasks to start the coro
+    await asyncio.sleep(0.01)  # allow time for tasks to start the coroutines
     assert started == set([])
     assert finished == set([])
 
     iterator = aiter(iterable)
-    await asyncio.sleep(0)  # allow time for tasks to start the coro
+    await asyncio.sleep(0.01)  # allow time for tasks to start the coroutines
     assert started == {1}
     assert finished == set([])
 
@@ -57,7 +57,7 @@ async def test_async_map_jobs_throttle():
     async for job_id in iterator:
         completed += 1
         assert job_id in finished
-        await asyncio.sleep(0.01)  # allow time for tasks to start the coro
+        await asyncio.sleep(0.01)  # allow time for tasks to start the coroutines
         assert len(started) == min(completed + 1, 3)
 
     assert started == {1, 2, 3}
