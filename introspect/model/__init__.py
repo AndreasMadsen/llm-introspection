00a28420e8727350c8ea27d3461110f429b3b2b5
Andreas Madsen <amwebdk@gmail.com>
2023-09-25 14:45:57 -0400

    set model directory as static

--- introspect/model/__init__.py
+++ introspect/model/__init__.py
@@ -2,13 +2,13 @@
 __all__ = ['FalconModel', 'Llama2Model',
            'models', 'AbstractModel']
 
-from typing import Type
+from typing import Type, Mapping
 
 from ._abstract_model import AbstractModel
 from .falcon import FalconModel
 from .llama2 import Llama2Model
 
-models: dict[str, Type[AbstractModel]] = {
+models: Mapping[str, Type[AbstractModel]] = {
     Model._name: Model
     for Model
     in [FalconModel, Llama2Model]
