8836adedd07dc3860fe5383323fbfd0d40cf07e2
Andreas Madsen <amwebdk@gmail.com>
2023-11-02 15:46:16 -0400

    wip: add MC tasks

--- introspect/tasks/__init__.py
+++ introspect/tasks/__init__.py
@@ -1,7 +1,8 @@
 
 __all__ = [
     'AbstractTask',
-    'SentimentClassifyTask', 'SentimentAnswerableTask', 'SentimentCounterfactualTask', 'SentimentRedactedTask',
+    'SentimentClassifyTask', 'SentimentAnswerableTask', 'SentimentCounterfactualTask', 'SentimentRedactedTask', 'SentimentImportanceTask',
+    'MultiChoiceClassifyTask',
     'tasks'
 ]
 
@@ -9,10 +10,12 @@ from typing import Type, Mapping
 
 from ._abstract_tasks import AbstractTask
 from .sentiment import SentimentClassifyTask, SentimentAnswerableTask, SentimentCounterfactualTask, SentimentRedactedTask, SentimentImportanceTask
+from .multi_choice import MultiChoiceClassifyTask
 from ..types import DatasetCategories, TaskCategories
 
 tasks: Mapping[tuple[DatasetCategories, TaskCategories], Type[AbstractTask]] = {
     (Task.dataset_category, Task.task_category): Task
     for Task
-    in [SentimentClassifyTask, SentimentAnswerableTask, SentimentCounterfactualTask, SentimentRedactedTask, SentimentImportanceTask]
+    in [SentimentClassifyTask, SentimentAnswerableTask, SentimentCounterfactualTask, SentimentRedactedTask, SentimentImportanceTask,
+        MultiChoiceClassifyTask]
 }
