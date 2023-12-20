9cde96c3b51eab8418b85ff6377f6dedbb6dd921
Andreas Madsen <amwebdk@gmail.com>
2023-12-20 10:57:37 +0900

    update plots for paper

---
+++ introspect/plot/tagger.py
@@ -0,0 +1,38 @@
+
+from typing import Literal
+
+def classify_persona(task_config: list[str]) -> Literal['human', 'you', 'objective']:
+    if 'c-persona-human' in task_config:
+        return 'human'
+    elif 'c-persona-you' in task_config:
+        return 'you'
+    else:
+        return 'objective'
+
+def classify_redact(task_config: list[str]) -> Literal['no-redact', 'removed', 'redacted']:
+    if 'c-no-redacted' in task_config:
+        return 'no-redact'
+    elif 'm-removed' in task_config:
+        return 'removed'
+    else:
+        return 'redacted'
+
+def explain_persona(task_config: list[str]) -> Literal['human', 'you', 'objective']|None:
+    if 'e-persona-human' in task_config and 'c-persona-human' in task_config:
+        return 'human'
+    elif 'e-persona-you' in task_config and 'c-persona-you' in task_config:
+        return 'you'
+    elif ('e-persona-you' not in task_config and 'c-persona-you' not in task_config and
+            'e-persona-human' not in task_config and 'c-persona-human' not in task_config):
+        return 'objective'
+
+    return None
+
+def explain_counterfactual_target(task_config: list[str]) -> Literal['implicit', 'explicit']:
+    if 'e-implcit-target' in task_config:
+        return 'implicit'
+    else:
+        return 'explicit'
+
+def explain_redact(task_config: list[str]) -> Literal['no-redact', 'removed', 'redacted']:
+    return classify_redact(task_config)
