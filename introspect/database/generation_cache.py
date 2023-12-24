a4816c240084fd7cafe046abb00558fb526c8e3e
Andreas Madsen <amwebdk@gmail.com>
2023-12-24 12:07:41 +0900

    fix experiments, after task simplification

--- introspect/database/generation_cache.py
+++ introspect/database/generation_cache.py
@@ -55,6 +55,10 @@ class GenerationCache(AbstractDatabase):
         self._cache_dir = cache_dir
         super().__init__(_database_to_filepath(database, cache_dir), **kwargs)
 
+    @staticmethod
+    def exists(database: str, cache_dir: Path):
+        return _database_to_filepath(database, cache_dir).exists()
+
     async def open(self) -> bool:
         is_new = await super().open()
 
