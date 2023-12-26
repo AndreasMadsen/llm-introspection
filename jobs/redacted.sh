4fcbb1938ce5a6085c4dd3020ac743b2e5130d27
Andreas Madsen <amwebdk@gmail.com>
2023-12-26 15:01:44 +0900

    add mistral to jobs

--- jobs/redacted.sh
+++ jobs/redacted.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source "jobs/_submitjob.sh"
 
-declare -A gpus=( ["llama2-70b"]="x4" ["llama2-7b"]="x1" ["falcon-40b"]="x4" ["falcon-7b"]="x1" )
-declare -A time=(   ["llama2-70b IMDB"]="14:00"  ["llama2-7b IMDB"]="1:00"   ["falcon-40b IMDB"]="1:00"   ["falcon-7b IMDB"]="1:00"
-                    ["llama2-70b RTE"]="6:00"    ["llama2-7b RTE"]="1:00"    ["falcon-40b RTE"]="1:00"    ["falcon-7b RTE"]="1:00"
-                    ["llama2-70b bAbI-1"]="6:00" ["llama2-7b bAbI-1"]="1:00" ["falcon-40b bAbI-1"]="1:00" ["falcon-7b bAbI-1"]="1:00"
-                    ["llama2-70b MCTest"]="6:00" ["llama2-7b MCTest"]="1:00" ["falcon-40b MCTest"]="1:00" ["falcon-7b MCTest"]="1:00")
+declare -A gpus=( ["llama2-70b"]="x4" ["llama2-7b"]="x1" ["falcon-40b"]="x4" ["falcon-7b"]="x1" ["mistral-v1-7b"]="x1" )
+declare -A time=( ["llama2-70b IMDB"]="14:00"  ["llama2-7b IMDB"]="3:00"   ["falcon-40b IMDB"]="14:00"  ["falcon-7b IMDB"]="3:00"   ["mistral-v1-7b IMDB"]="3:00"
+                  ["llama2-70b RTE"]="6:00"    ["llama2-7b RTE"]="3:00"    ["falcon-40b RTE"]="6:00"    ["falcon-7b RTE"]="3:00"    ["mistral-v1-7b RTE"]="3:00"
+                  ["llama2-70b bAbI-1"]="6:00" ["llama2-7b bAbI-1"]="3:00" ["falcon-40b bAbI-1"]="6:00" ["falcon-7b bAbI-1"]="3:00" ["mistral-v1-7b bAbI-1"]="3:00"
+                  ["llama2-70b MCTest"]="6:00" ["llama2-7b MCTest"]="3:00" ["falcon-40b MCTest"]="6:00" ["falcon-7b MCTest"]="3:00" ["mistral-v1-7b MCTest"]="3:00")
 
-for model_name in 'llama2-70b' 'llama2-7b' 'falcon-40b' 'falcon-7b'
+for model_name in 'llama2-70b' 'llama2-7b' 'falcon-40b' 'falcon-7b' 'mistral-v1-7b'
 do
     for dataset in 'IMDB' 'RTE' 'bAbI-1' 'MCTest'
     do
