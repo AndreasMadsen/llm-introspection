2ae9630357a67ddcbb8ea4c2f2fd39022f03bfe5
Andreas Madsen <amwebdk@gmail.com>
2024-01-17 07:40:47 -0500

    final modifications

--- jobs/importance.sh
+++ jobs/importance.sh
@@ -2,7 +2,7 @@
 source "jobs/_submitjob.sh"
 
 declare -A gpus=( ["llama2-70b"]="x4" ["llama2-7b"]="x1" ["falcon-40b"]="x4" ["falcon-7b"]="x1" ["mistral-v1-7b"]="x1" )
-declare -A time=( ["llama2-70b IMDB"]="14:00"  ["llama2-7b IMDB"]="3:00"   ["falcon-40b IMDB"]="14:00"  ["falcon-7b IMDB"]="3:00"   ["mistral-v1-7b IMDB"]="3:00"
+declare -A time=( ["llama2-70b IMDB"]="14:00"  ["llama2-7b IMDB"]="3:00"   ["falcon-40b IMDB"]="14:00"  ["falcon-7b IMDB"]="10:00"   ["mistral-v1-7b IMDB"]="3:00"
                   ["llama2-70b RTE"]="6:00"    ["llama2-7b RTE"]="3:00"    ["falcon-40b RTE"]="6:00"    ["falcon-7b RTE"]="3:00"    ["mistral-v1-7b RTE"]="3:00"
                   ["llama2-70b bAbI-1"]="6:00" ["llama2-7b bAbI-1"]="3:00" ["falcon-40b bAbI-1"]="6:00" ["falcon-7b bAbI-1"]="3:00" ["mistral-v1-7b bAbI-1"]="3:00"
                   ["llama2-70b MCTest"]="6:00" ["llama2-7b MCTest"]="3:00" ["falcon-40b MCTest"]="6:00" ["falcon-7b MCTest"]="3:00" ["mistral-v1-7b MCTest"]="3:00")
