195c29806b37a1a21c47b436a38223923f7df447
Andreas Madsen <amwebdk@gmail.com>
2024-02-05 11:43:44 -0500

    document I/O code

--- introspect/client/test.py
+++ introspect/client/test.py
@@ -15,10 +15,23 @@ def _dict_to_callable(response: dict[str, str|None]) -> Callable[[str], Awaitabl
     return fn
 
 class TestClient(AbstractClient[TestInfo]):
+    """This is a mock client, only useful for testing.
+
+    This client can create un-cached responses without a server. The response
+    are generated using the `response` argument.
+    """
     _response: Callable[[str], Awaitable[str|None]]
 
     def __init__(self, response: Callable[[str], Awaitable[str|None]]|dict[str, str|None] = {},
                        cache: GenerationCache | None = None) -> None:
+        """Creates a TestClient used for mocking a server.
+
+        Args:
+            response (Callable[[str], Awaitable[str | None]] | dict[str, str | None], optional):
+                This can either be an async function (prompt: str) -> response: str. Or,
+                it can be a dict[prompt, response]. Defaults to {}.
+            cache (GenerationCache | None, optional): _description_. Defaults to None.
+        """
         self.log = []
 
         if isinstance(response, dict):
